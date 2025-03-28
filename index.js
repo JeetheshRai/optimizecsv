37875697385079039d6eecc2c6ba2ff4dcae8468
unknown <jeetheshrai6@gmail.com>
2025-03-28 10:44:50 -0700

    bug fixes

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { database , mysql } = require('./database');
 const ReportService = require('./services/report.service');
 const file_streamUtil = require('./utils/file_stream.util');
     (async () => {
-        await mysql.initMysql()
-        await database.start()
+        if(config.generate_csv_from=='mysql')await mysql.initMysql()
+        if(config.generate_csv_from=='mongodb')await database.start()
     if (!file_streamUtil.existsSync('./uploads')) {
         await file_streamUtil.createFolder('./uploads', { recursive: true });
         console.log('Uploads Folder created.');
